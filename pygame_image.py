3892a1110f84136517101c1fd69e4cb258a28b8f
c0a2205337 <c0a2205337@edu.teu.ac.jp>
2023-11-21 15:57:30 +0900

    練習6

--- pygame_image.py
+++ pygame_image.py
@@ -10,14 +10,19 @@ def main():
     koukaton = pg.image.load("ex01/fig/3.png")
     koukaton = pg.transform.flip(koukaton, True, False)
     koukaton_10 = pg.transform.rotozoom(koukaton, 10, 1.0)
-    kk_list = [koukaton, koukaton_10]
+    koukaton_20 = pg.transform.rotozoom(koukaton, 20, 1.0)
+    kk_list = [koukaton, koukaton_10, koukaton_20, koukaton_10]
+    # bg_imgs = []
     tmr = 0
+    
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        screen.blit(bg_img, [0, 0])
-        screen.blit(kk_list[tmr%2], [300, 200])
+        x = tmr%1600
+        screen.blit(bg_img, [-x, 0])
+        screen.blit(bg_img, [1600-x, 0])
+        screen.blit(kk_list[tmr%4], [300, 200])
         pg.display.update()
         tmr += 1        
         clock.tick(10)
