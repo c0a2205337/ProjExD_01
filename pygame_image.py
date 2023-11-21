365cce64997cd0d39f8d2e84e408866aad888fa9
c0a2205337 <c0a2205337@edu.teu.ac.jp>
2023-11-21 15:43:52 +0900

    練習5

--- pygame_image.py
+++ pygame_image.py
@@ -11,18 +11,13 @@ def main():
     koukaton = pg.transform.flip(koukaton, True, False)
     koukaton_10 = pg.transform.rotozoom(koukaton, 10, 1.0)
     kk_list = [koukaton, koukaton_10]
-    kk_change = 0
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
         screen.blit(bg_img, [0, 0])
-        if kk_change:
-            kk_change = False
-        else:
-            kk_change = True
-        screen.blit(kk_list[kk_change], [300, 200])
+        screen.blit(kk_list[tmr%2], [300, 200])
         pg.display.update()
         tmr += 1        
         clock.tick(10)
