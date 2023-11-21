0bd9867d5b0c09029a5689e26d8b9ebcf6a7f18a
c0a2205337 <c0a2205337@edu.teu.ac.jp>
2023-11-21 16:33:29 +0900

    演習2

--- pygame_image.py
+++ pygame_image.py
@@ -10,22 +10,27 @@ def main():
     koukaton = pg.image.load("ex01/fig/3.png")
     koukaton = pg.transform.flip(koukaton, True, False)
     bg_img2 = pg.transform.flip(bg_img, True, False)
-    koukaton_10 = pg.transform.rotozoom(koukaton, 5, 1.0)
-    koukaton_20 = pg.transform.rotozoom(koukaton, 10, 1.0)
-    kk_list = [koukaton, koukaton_10, koukaton_20, koukaton_10]
+    koukaton_3 = pg.transform.rotozoom(koukaton, 5, 1.0)
+    koukaton_5 = pg.transform.rotozoom(koukaton, 5, 1.0)
+    koukaton_10 = pg.transform.rotozoom(koukaton, 10, 1.0)
+    koukaton_20 = pg.transform.rotozoom(koukaton, 20, 1.0)
+    
+    kk_list = [koukaton, koukaton_3, koukaton_5, koukaton_10, koukaton_20, koukaton_20, koukaton_10, koukaton_5, koukaton_3, koukaton]
     tmr = 0
     
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-
-        x = tmr%1600
+        x = tmr%3200
         screen.blit(bg_img, [-x, 0])
         screen.blit(bg_img2, [1600-x, 0])
-        screen.blit(kk_list[tmr%4], [300, 200])
+        screen.blit(bg_img, [3200-x, 0])
+            
+
+        screen.blit(kk_list[tmr%9], [300, 200])
         pg.display.update()
         tmr += 1        
-        clock.tick(10)
+        clock.tick(100)
 
 
 if __name__ == "__main__":
